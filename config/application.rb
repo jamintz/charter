ab13bedd5db00790044c4a513e2319b8af14143c
Jason Mintz <jasonamintz@gmail.com>
2017-05-01 16:20:05 -0400

    fp

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Charter
   class Application < Rails::Application
-    config.filepicker_rails.api_key = ENV['FILEPICKER_API_KEY']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
---
+++ db/migrate/20170501201902_add_fp.rb
@@ -0,0 +1,6 @@
+class AddFp < ActiveRecord::Migration[5.0]
+  def change
+       add_column transactions, :filepicker_field, :string
+       add_column attributes, :filepicker_field, :string
+  end
+end
