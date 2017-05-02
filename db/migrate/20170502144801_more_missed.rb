fce15dedf40186292d09c049c57ee2bd26817db4
Jason Mintz <jasonamintz@gmail.com>
2017-05-02 10:54:56 -0400

    oops

--- db/migrate/20170502144801_more_missed.rb
+++ db/migrate/20170502144801_more_missed.rb
@@ -1,10 +1,10 @@
 class MoreMissed < ActiveRecord::Migration[5.0]
   def change
     unless ActiveRecord::Base.connection.column_exists?(:transactions, :client_id)
-      add_column :clients, :attr_url, :string
+      add_column :transactions, :client_id, :string
     end
     unless ActiveRecord::Base.connection.column_exists?(:attrs, :client_id)
-      add_column :clients, :attr_url, :string
+      add_column :atts, :client_id, :string
     end
   end
 end
